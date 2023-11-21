47bf69c539dcb1b75b313b6617c8a793967d7a43
Ariiq354 <realgoldcraft@gmail.com>
2023-11-21 12:47:55 +0700

    hehe

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={cn(courier.className, "bg-[url('/BG.jpg')]")}>
         {children}
       </body>
